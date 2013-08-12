e16abc02784786490dda0e12341ba8370b19d5d3
Martin Carlberg <martin@carlberg.org>
2013-08-12 09:32:42 +0200

    Updated version

--- acorn.js
+++ acorn.js
@@ -33,7 +33,7 @@
 })(function(exports) {
   "use strict";
 
-  exports.version = "0.3.2.1";
+  exports.version = "0.3.3-objj-1";
 
   // The main exported interface (under `self.acorn` when in the
   // browser) is a `parse` function that takes a code string and
